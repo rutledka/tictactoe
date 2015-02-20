1559faf60d7a0e0b890833d1d6369572791edb16
rutledka <khalilrutledge@gmail.com>
2015-02-19 22:47:48 -0500

    semi-finished update with do/while

--- tictactoe.js
+++ tictactoe.js
@@ -11,28 +11,24 @@ function ticTacToe() {
 	//var playerTwo = new self.player("Player 2", "o");
 	
 	//What's the difference between:
-	playerOne: new self.player("Player 1", "x");
-	playerTwo: new self.player("Player 2", "o");
+	//playerOne: new self.player("Player 1", "x");
+	//playerTwo: new self.player("Player 2", "o");
 
-	var currentRound = 0;
-	var currentPlayer;
-	var occupied = [];
-
-	self.init = function(){
-		currentPlayer = currentRound % 2 ? self.playerTwo : self.playerOne;
-		currentRound++;
-
-	}
+	//var currentRound = 0;
+	//var currentPlayer;
+	self.occupied = new Array();
 
 	self.placeMark = function(squareID, player){
-		occupied.push([squareID, player.playerClass]);
-		player.moves.push(squareID);
-		console.log(occupied);
-		console.log(player.moves);
-		self.checkValues(player);
+		if(!self.isOccupied()){
+			b.occupied.push([squareID, player.playerClass]);
+			player.moves.push(squareID);
+			console.log(b.occupied);
+			console.log(player.moves);
+			self.hasWinner(player);
+		}
 	}
 
-	self.checkValues = function(player){
+	self.hasWinner = function(player){
 		//Hardcoded the winning possibilities
 		var winners = [
 		[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
@@ -60,17 +56,50 @@ function ticTacToe() {
 		return false;
 	}
 
-	self.resetBoard = function(){
-		playerOne.moves = [];
-		playerTwo.moves = [];
-		currentRound = 0;
-		occupied = [];
-
+	self.hasMoves = function(){
+		if(self.occupied.length == 9){
+			console.log("board full");
+			return false;
+		} else {
+			return true;
+		}
+		
 	}
 
-	self.init();
+	self.isOccupied = function(squareID){
+		for(var i=0;  i<self.occupied.length; i++){
+			if(self.occupied[i][0] == squareID){
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	self.resetBoard = function(){
+		self.playerOne.moves = [];
+		self.playerTwo.moves = [];
+		self.currentRound = 0;
+		self.occupied = [];
+
+	}
 	
 }
+
+var b = new ticTacToe();
+b.playerOne = new b.player("Player One", "x");
+b.playerTwo = new b.player("Player Two", "o");
+b.currentRound = 0;
+
+//
+do {
+	var player = b.currentRound % 2 ? b.playerTwo : b.playerOne;
+	var cell = Math.floor(Math.random()*9);
+
+	b.placeMark(cell,player);
+	console.log(b.currentRound + " " + cell + " " + player);
+	b.currentRound++;
+} while(b.hasMoves());
 
 
 
